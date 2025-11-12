aa2619f5b837c0f0a6523b0ed66521d34cb0d637
Nadayahia_dev <nadayahiadigitalart-dev>
2025-11-12 22:33:02 +0200

    doing animation

--- nada-portfolio/src/components/Pcard.jsx
+++ nada-portfolio/src/components/Pcard.jsx
@@ -19,17 +19,21 @@ const Pcard = (props) => {
                  <Tag tag3="prototype"/>
                  <Tag tag4="design system"/> */}
 
-                 
-
-                
-              
+    <div className="tags_layout">
+    <p className="tag">{props.tag1}</p>
+     <p className="tag">{props.tag2}</p>
+     <p className="tag">{props.tag3}</p>
+     <p className="tag">{props.tag4}</p>
+     <p className="tag">{props.tag5}</p> 
+     </div>
+      
                <br></br><br></br>
               </div>
               <a className="a" href="UXUI_Projectdetails1.html">
                <Button button="Explore Work"/>
               </a>
             </div>
-            <img  src={props.img} alt="ux_ui_mockup_card" />
+            <img className="siZe" src={props.img} alt="ux_ui_mockup_card" />
           </section>
 
 
--- nada-portfolio/src/components/ProjectSection.jsx
+++ nada-portfolio/src/components/ProjectSection.jsx
@@ -1,31 +1,78 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
         // import React from "react";
+        import React, { useRef } from "react";
+
         import showreelVideo from "../assets/Nad Yahia-22-101081-showreel2024--.mp4";
+      import useHorizontalScroll from "../useHorizontalScroll";
+      const scrollRef = useHorizontalScroll();
         import uxui from "../assets/uxui_home_card.png";
+        import D from "../assets/3D booth card.png";
+        import F from "../assets/dev_card.png";
+        import G from "../assets/graphic_design_tshirt_card.png";
+
+
+
        
         
         import './ProjectSection';
 import Pcard from './Pcard';
+import Button from './Button';
 
 const ProjectSection = () => {
+
+   const sectionRef = useRef(null);
+  const scrollerRef = useRef(null);
+
+  useHorizontalScroll(sectionRef, scrollerRef);
+
+
+
     return ( 
         <>
 
 
-    <main className="projects">
+    <main ref={scrollRef}  className="projects">
       <p className="f32">What services I offer ?</p>
       <section className="projects-section">
         <article className="horizontal-scroll">
          
-            <Pcard  t="ux ui design" img={uxui} tags={["Wireframe", "Prototype", "UI Design", "Research"]} />
-            <Pcard  t="3d design" img={uxui}  />
+            <Pcard  t="UX UI Design" img={uxui} tag1="UX Research" tag2="ui design"
+            tag3="wireframe" tag4="prototype" tag5="design system" />
 
-          {/* <section className="proj_cards">
-            <video autoPlay muted loop playsInline width="500" height="300" controls>
-              <source src={showreelVideo} type="video/mp4" />
-            </video>
-          </section> */}
+            <Pcard  t="3D Design" img={D} alt="Front-end_ux ui" tag1="Product Design"
+            tag2="Interior design" tag3="AR" />
+
+              <Pcard  t="Front-end Development" img={F} tag1="HTML"
+            tag2="CSS" tag3="JavaScript" tag4="React" />
+
+             <Pcard  t="Graphic Design" className="siZe" img={G} tag1="Illustrations"
+            tag2="Social Media" tag3="Branding" />
+
+              <section className="proj_cards">
+
+<div class="x">
+            <div class="card_title">
+                <p class="f32">Motion Graphics </p>
+                <div class="tags_layout">
+                    <p class="tag">Reels</p>
+                    <p class="tag"> Social media</p>
+                    <p class="tag">2D</p>
+                    <p class="tag">3D </p>
+    
+                </div>
+               <br></br> 
+            </div>
+                    <Button button="Explore work"/>
+</div>
+                <video autoplay muted loop playsinline width="500" height="300" controls id="bg-video" >
+  <source src="photos/Nad Yahia-22-101081-showreel2024--.mp4" type="video/mp4"/>
+  Your browser does not support the video tag.
+</video>
+                    
+            </section>
+
+        
         </article>
       </section>
     </main> 
@@ -42,33 +89,4 @@ export default ProjectSection;
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
